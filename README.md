e5af4c519d94116c1dad19a86129ac07ac7a1c70
herrylegowo <herrylegowo19@gmail.com>
2020-04-12 17:08:03 +0800

    commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Complete-login-system
---
+++ application/controllers/Auth.php
@@ -0,0 +1,52 @@
+<?php
+class Auth extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Login Page';
+        $this->load->view('template/auth_header', $data);
+        $this->load->view('auth/login');
+        $this->load->view('template/auth_footer');
+    }
+
+    public function registration()
+    {
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'This email is already registered !'
+        ]);
+        $this->form_validation->set_rules('password1', 'password', 'required|trim|min_length[5]|matches[password2]', [
+            'matches' => 'Passwword dont match!',
+            'min_length' => 'password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'password', 'required|trim|matches[password1]');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'WPU Registration';
+            $this->load->view('template/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('template/auth_footer');
+        } else {
+            $data = [
+                'name' => htmlspecialchars($this->input->post('name')),
+                'email' => htmlspecialchars($this->input->post('email')),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+
+            ];
+
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation your account hass been created. Please Login</div>');
+            redirect('auth');
+        }
+    }
+}
