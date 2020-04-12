b91e9778c86d40d175de5e4e7827537a9ec7a031
herrylegowo <herrylegowo19@gmail.com>
2020-04-13 02:39:35 +0800

    user & admin page

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,16 +1,16 @@
 <?php
-class User extends CI_Controller
+class Admin extends CI_Controller
 {
     public function index()
     {
-        $data['title'] = 'My Profil';
+        $data['title'] = 'Dashbord';
         $data['user'] = $this->db->get_where('user', ['email' =>
         $this->session->userdata('email')])->row_array();
 
         $this->load->view('template/header', $data);
         $this->load->view('template/sidebar', $data);
         $this->load->view('template/topbar', $data);
-        $this->load->view('user/index', $data);
+        $this->load->view('admin/index', $data);
         $this->load->view('template/footer');
     }
 }
