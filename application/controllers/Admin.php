983178a5db7414cab9b002887d7c8b6032a36678
herrylegowo <herrylegowo19@gmail.com>
2020-04-16 00:41:02 +0800

    blocked page

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,6 +1,14 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
 class Admin extends CI_Controller
 {
+
+    public function __construct()
+    {
+        parent::__construct();
+        check_login();
+    }
+
     public function index()
     {
         $data['title'] = 'Dashboard';
--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -1,6 +1,12 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
 class Menu extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        check_login();
+    }
     public function index()
     {
         $data['title'] = 'Menu Management';
