468a938bbdc0216cf9b60bf60206c25456dcf493
herrylegowo <herrylegowo19@gmail.com>
2020-04-19 03:34:56 +0800

    Access Management

--- application/helpers/mhl_helper.php
+++ application/helpers/mhl_helper.php
@@ -19,3 +19,19 @@ function check_login()
         }
     }
 }
+
+function check_access($role_id, $menu_id)
+{
+    $ci = get_instance();
+
+    $result = $ci->db->get_where(
+        'user_access_menu',
+        [
+            'role_id' => $role_id,
+            'menu_id' => $menu_id
+        ]
+    );
+    if ($result->num_rows() > 0) {
+        return "checked = 'checked'";
+    }
+}
