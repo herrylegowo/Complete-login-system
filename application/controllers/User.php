e94cb63f7af0b6a29fb798b538c2f2480a183d8f
herrylegowo <herrylegowo19@gmail.com>
2020-04-20 04:22:02 +0800

    Edit Profil

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -61,4 +61,52 @@ class User extends CI_Controller
             }
         }
     }
+
+    public function edit()
+    {
+        $data['title'] = 'Edit profil';
+        $data['user'] = $this->db->get_where('user', ['email' =>
+        $this->session->userdata('email')])->row_array();
+
+        $this->form_validation->set_rules('name', 'Full Name', 'required|trim');
+
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('template/header', $data);
+            $this->load->view('template/sidebar', $data);
+            $this->load->view('template/topbar', $data);
+            $this->load->view('user/edit', $data);
+            $this->load->view('template/footer');
+        } else {
+            $name = $this->input->post('name');
+            $email = $this->input->post('email');
+
+            // cek gambar
+            $upload_image = $_FILES['image'];
+            if ($upload_image) {
+                $config['allowed_types'] = 'gif|jpg|png';
+                $config['max_size']     = '4000';
+                $config['upload_path'] = './assets/img/profile/';
+
+                $this->load->library('upload', $config);
+
+                if ($this->upload->do_upload('image')) {
+                    $old_image = $data['user']['image'];
+                    if ($old_image != 'default.jpg') {
+                        unlink(FCPATH . 'assets/img/profile/' . $old_image);
+                    }
+                    $new_image = $this->upload->data('file_name');
+                    $this->db->set('image', $new_image);
+                } else {
+                    echo $this->upload->display_errors();
+                }
+            }
+
+            $this->db->set('name', $name);
+            $this->db->where('email', $email);
+            $this->db->update('user');
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Your Profil is Updated!</div>');
+            redirect('user');
+        }
+    }
 }
